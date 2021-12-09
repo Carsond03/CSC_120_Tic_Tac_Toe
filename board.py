98b36bd39b214c3f3fe287db9748ca4a6baf84e5
Samuel Deckard <cfdeckard@my.waketech.edu>
2021-12-09 13:59:56 -0500

    this is working code for stage 3 of tic tac toe

--- board.py
+++ board.py
@@ -3,9 +3,59 @@ def print_board():
     print(board[0])
     print(board[1])
     print(board[2])
+def check_mark():
+    for i in range(0, 3, 1):
+        for c in range(0, 3, 1):
+            if board[i][c] == "-":
+                return True
+    return False
+def check_win():
+    for r in range(0, 3, 1):
+        if board[r][0] == "X" and board[r][1] == "X" and board[r][2] == "X":
+            print("Player 1, is the winner")
+            return True
+    for r in range(0, 3, 1):
+        if board[r][0] == "O" and board[r][1] == "O" and board[r][2] == "O":
+            print("Player 2, is the winner")
+            return True
+    for c in range(0, 3, 1):
+        if board[0][c] == "X" and board[1][c] == "X" and board[2][c] == "X":
+            print("player 1, is the winner")
+            return True
+    for c in range(0, 3, 1):
+        if board[0][c] == "O" and board[1][c] == "O" and board[2][c] == "O":
+            print("player 2, is the winner")
+            return True
+    if board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+        print("Player 1, is the winner")
+        return True
+    if board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
+        print("Player 1, is the winner")
+        return True
+    if board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+        print("Player 2, is the winner")
+        return True
+    if board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+        print("Player 2, is the winner")
+        return True
+    return False
+
+
+
+
+
+
+
 print_board()
 player = 1
 while True:
+    if check_mark() == False:
+        print("no more moves on the board")
+        if check_win() == False:
+            print("draw")
+            break
+
+        break
     print("player {}, make your move".format(player))
     while True:
         while True:
@@ -35,4 +85,6 @@ while True:
         player = 2
     else:
         player = 1
+    if check_win() == True:
+        break
 
